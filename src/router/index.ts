174f180b23209c1e6ac18cf72ddcfc37678c2c1f
Yuzu <1581523609@qq.com>
2024-11-06 17:26:58 +0800

    feat(router): 优化路由配置并添加静态路由模块
    - 重构路由配置，支持 hash 和 history 两种模式
    - 新增静态路由模块，包括布局路由和错误路由
    - 添加路由滚动重置功能
    - 优化路由文件结构，提高可维护性

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+  type RouterHistory,
+} from 'vue-router'
+import { errorRouter, layoutRouter } from './modules/staticRouter'
+
+// .env 配置文件读取
+const mode = import.meta.env.VITE_ROUTER_MODE
+
+// 路由模式
+const routerMode: RouterMode = {
+  hash: () => createWebHashHistory(),
+  history: () => createWebHistory(),
+}
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  ]
+  history: routerMode[mode](),
+  routes: [...layoutRouter, ...errorRouter],
+  scrollBehavior() {
+    return { left: 0, top: 0 }
+  },
 })
 
 export default router
+
+interface RouterMode {
+  [key: string]: () => RouterHistory
+}
---
+++ src/router/modules/staticRouter.ts
@@ -0,0 +1,46 @@
+import type { RouteRecordRaw } from 'vue-router'
+import Layout from '@/layout/index.vue'
+
+export const layoutRouter: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'layout',
+    component: Layout,
+    redirect: '/home',
+    children: [],
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/index.vue'),
+    meta: {
+      title: '登录',
+    },
+  },
+]
+
+export const errorRouter: RouteRecordRaw[] = [
+  {
+    path: '/403',
+    name: '403',
+    component: () => import('@/views/error/403.vue'),
+    meta: {
+      title: '403页面',
+    },
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('@/views/error/404.vue'),
+    meta: {
+      title: '404页面',
+    },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('@/views/error/404.vue'),
+    meta: {
+      title: '404页面',
+    },
+  },
+]
