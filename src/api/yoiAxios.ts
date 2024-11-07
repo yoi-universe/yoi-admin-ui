4089208efc12b1cb2960382b3a448525def04f60
Yuzu <1581523609@qq.com>
2024-11-07 16:50:03 +0800

    feat(api): 重构 yoiAxios 并添加 loading 功能
    - 继承 AbsAxios 类，扩展功能
    - 添加 loading 相关属性和方法
    - 重写请求方法，支持 loading 配置
    - 新增 ElMessageHandler 类处理消息显示
    - 优化代码结构，提高可维护性

--- src/api/yoiAxios.ts
+++ src/api/yoiAxios.ts
@@ -1,10 +1,182 @@
-import Yoi from '@/utils/axios'
-import type { InternalAxiosRequestConfig } from 'axios'
+import AbsAxios, {
+  type MessageHandler,
+  type Options,
+  type Result,
+} from '@/utils/axios'
+import type { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import { ElLoading, ElMessage, type LoadingOptions } from 'element-plus'
 
-class YoiAxios extends Yoi {
+class YoiAxios extends AbsAxios {
+  public options: IOptions = {}
+  private loading: LoadingOptions = {}
+  private loadingInstance: LoadingInstance = {
+    target: null,
+    count: 0,
+  }
+
+  constructor() {
+    super(undefined, {
+      showMessage(
+        message: string,
+        type: 'error' | 'success' | 'info' | 'warning',
+      ) {
+        ElMessage({
+          message,
+          type,
+          plain: true,
+        })
+      },
+    })
+  }
+
   public override onRequestFulfilled(config: InternalAxiosRequestConfig) {
     super.onRequestFulfilled(config)
-
+    // 创建loading实例
+    if (this.options.loading) {
+      this.loadingInstance.count++
+      if (this.loadingInstance.count === 1) {
+        this.loadingInstance.target = ElLoading.service(this.loading)
+      }
+    }
     return config
   }
+
+  public override onResponseFulfilled(response: AxiosResponse) {
+    if (this.options.loading) this.closeLoading()
+    return super.onResponseFulfilled(response)
+  }
+
+  public override onResponseRejected(error: AxiosResponse) {
+    if (this.options.loading) this.closeLoading()
+    return super.onResponseRejected(error)
+  }
+
+  /**
+   * 关闭Loading层实例
+   */
+  private closeLoading() {
+    if (this.options.loading && this.loadingInstance.count > 0)
+      this.loadingInstance.count--
+    if (this.loadingInstance.count === 0) {
+      this.loadingInstance.target.close()
+      this.loadingInstance.target = null
+    }
+  }
+
+  /**
+   * Get 请求
+   * @param url 请求地址
+   * @param params 请求参数
+   * @param options 配置项
+   * @param loading 配置项
+   * @returns Promise<T>
+   */
+  public override get<T = Result>(
+    url: string,
+    params?: object,
+    options?: IOptions,
+    loading?: LoadingOptions,
+  ): Promise<T> {
+    this.options = options || {}
+    this.loading = loading || {}
+    return super.get(url, params, options)
+  }
+
+  /**
+   * Post 请求
+   * @param url 请求地址
+   * @param data 请求参数
+   * @param options 配置项
+   * @param loading 配置项
+   * @returns Promise<T>
+   */
+  public override post<T = Result>(
+    url: string,
+    data?: object,
+    options?: IOptions,
+    loading?: LoadingOptions,
+  ): Promise<T> {
+    this.options = options || {}
+    this.loading = loading || {}
+    return super.post(url, data, options)
+  }
+
+  /**
+   * Put 请求
+   * @param url 请求地址
+   * @param data 请求参数
+   * @param options 配置项
+   * @param loading 配置项
+   * @returns Promise<T>
+   */
+  public override put<T = Result>(
+    url: string,
+    data?: object,
+    options?: IOptions,
+    loading?: LoadingOptions,
+  ): Promise<T> {
+    this.options = options || {}
+    this.loading = loading || {}
+    return super.put(url, data, options)
+  }
+
+  /**
+   * Delete 请求
+   * @param url 请求地址
+   * @param data 请求参数
+   * @param options 配置项
+   * @param loading 配置项
+   * @returns Promise<T>
+   */
+  public override delete<T = Result>(
+    url: string,
+    data?: object,
+    options?: IOptions,
+    loading?: LoadingOptions,
+  ): Promise<T> {
+    this.options = options || {}
+    this.loading = loading || {}
+    return super.delete(url, data, options)
+  }
+
+  /**
+   * 上传文件
+   * @param url 接口地址
+   * @param data 请求参数
+   * @param options 配置参数
+   * @param loading 加载loading参数
+   * @returns Promise<T>
+   */
+  public override upload<T = Result>(
+    url: string,
+    data?: object,
+    options?: IOptions,
+    loading?: LoadingOptions,
+  ): Promise<T> {
+    this.options = options || {}
+    this.loading = loading || {}
+    return super.upload(url, data)
+  }
+}
+
+export default new YoiAxios()
+
+class ElMessageHandler implements MessageHandler {
+  showMessage(message: string, type: 'error' | 'success' | 'info' | 'warning') {
+    ElMessage({
+      message,
+      type,
+      plain: true,
+    })
+  }
+}
+
+interface LoadingInstance {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  target: any
+  count: number
+}
+
+interface IOptions extends Options {
+  loading?: boolean
 }
