2e50fcc305b73c6e19886acd3ca7e8c10ab7654f
Yuzu <1581523609@qq.com>
2024-11-07 17:36:49 +0800

    feat(vite): 配置 Vite 代理和环境变量
    - 引入 loadEnv 函数以支持环境变量加载
    - 添加服务器配置，包括主机、端口、热更新等设置
    - 配置代理以解决跨域问题
    - 优化 Vite 配置结构，支持命令和模式参数

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    server: {
+      host: '0.0.0.0', // 允许本机IP访问 0.0.0.0
+      port: 5173, // 端口号
+      hmr: true, // 热更新
+      open: false, // 自动打开
+      proxy: {
+        // 代理跨域
+        [env.VITE_API_PREFIX]: {
+          // 配置哪个环境下的
+          target: env.VITE_BASE_URL,
+          rewrite: path =>
+            path.replace(new RegExp('^' + env.VITE_API_PREFIX), ''), // 路径重写
+          // 允许跨域
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
