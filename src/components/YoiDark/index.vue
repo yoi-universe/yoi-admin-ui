0bf45fec39bf956f0f3a668f5a2abbd3f62336da
Yuzu <1581523609@qq.com>
2024-11-08 17:51:26 +0800

    feat(components): 添加 YoiDark 组件
    添加 YoiDark 组件，用于切换页面主题（明暗模式）。组件通过按钮触发主题切换，并使用 view transition API 实现平滑过渡效果。
    
    - 支持通过插槽自定义按钮样式
    - 兼容不支持 view transition API 的浏览器
    - 提供自定义动画样式选项

---
+++ src/components/YoiDark/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="btn">
+    <span @click="handleSwitchDark">
+      <slot name="btn"></slot>
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const isDark = ref(false)
+
+const switchTheme = () => {
+  const html = document.documentElement
+  if (isDark.value) html.classList.add('dark')
+  else html.classList.remove('dark')
+}
+
+const handleSwitchDark = (event: MouseEvent) => {
+  const x = event.clientX
+  const y = event.clientY
+  // 计算按钮到最远点的距离用作裁剪圆形的半径
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y)
+  )
+
+  if (document.startViewTransition == undefined) {
+    console.log('你的浏览器不支持view transition')
+    // 切换主题逻辑
+    isDark.value = !isDark.value
+    switchTheme()
+  } else {
+    const transition = document.startViewTransition(() => {
+      // 切换主题逻辑
+      isDark.value = !isDark.value
+      switchTheme()
+    })
+    transition.ready.then(() => {
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`,
+      ];
+      document.documentElement.animate(
+        { clipPath: isDark.value ? clipPath : [...clipPath].reverse() },
+        {
+          duration: 400,
+          easing: "ease-in",
+          pseudoElement: isDark.value
+            ? "::view-transition-new(root)"
+            : "::view-transition-old(root)",
+        }
+      )
+    })
+  }
+
+}
+</script>
+
+<style lang="scss">
+/* 可选择的自定义动画样式 */
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root) {
+  z-index: 9999;
+}
+
+::view-transition-new(root) {
+  z-index: 1;
+}
+
+.dark::view-transition-old(root) {
+  z-index: 1;
+}
+
+.dark::view-transition-new(root) {
+  z-index: 9999;
+}
+
+.dark {
+  color-scheme: dark;
+}
+</style>
