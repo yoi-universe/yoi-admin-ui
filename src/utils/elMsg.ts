330ae27ee51c4f755e01f9be75134cd5ca6e867a
Yuzu <1581523609@qq.com>
2024-11-09 17:53:20 +0800

    feat(utils): 优化 elMsg 消息提示功能
    - 新增通用 elMsg 函数，支持自定义类型和持续时间
    - 在成功和错误提示函数中添加 duration 参数
    - 优化参数顺序，提高函数易用性

--- src/utils/elMsg.ts
+++ src/utils/elMsg.ts
@@ -1,13 +1,41 @@
-import { ElMessage } from 'element-plus'
+import { ElMessage, type messageType } from 'element-plus'
+
+/**
+ * el-message 全局提示
+ * @param message 消息文字
+ * @param type 类型
+ * @param duration 持续时间
+ * @param plain 是否纯色
+ * @param dangerouslyUseHTMLString 是否将 message 属性作为 HTML 片段处理
+ */
+export function elMsg(
+  message: string,
+  type: messageType = 'info',
+  duration: number = 3000,
+  plain: boolean = true,
+  dangerouslyUseHTMLString: boolean = false,
+) {
+  ElMessage.closeAll()
+  ElMessage({
+    type,
+    message,
+    duration,
+    showClose: true,
+    plain,
+    dangerouslyUseHTMLString,
+  })
+}
 
 /**
  * el-message 成功提示
  * @param message 消息文字
+ * @param duration 持续时间
  * @param plain 是否纯色
  * @param dangerouslyUseHTMLString 是否将 message 属性作为 HTML 片段处理
  */
 export function elMsgSuccess(
   message: string,
+  duration: number = 3000,
   plain: boolean = true,
   dangerouslyUseHTMLString: boolean = false,
 ) {
@@ -15,6 +43,7 @@ export function elMsgSuccess(
   ElMessage({
     type: 'success',
     message,
+    duration,
     showClose: true,
     plain,
     dangerouslyUseHTMLString,
@@ -24,11 +53,13 @@ export function elMsgSuccess(
 /**
  * el-message 错误提示
  * @param message 消息文字
+ * @param duration 持续时间
  * @param plain 是否纯色
  * @param dangerouslyUseHTMLString 是否将 message 属性作为 HTML 片段处理
  */
 export function elMsgError(
   message: string,
+  duration: number = 3000,
   plain: boolean = true,
   dangerouslyUseHTMLString: boolean = false,
 ) {
@@ -36,6 +67,7 @@ export function elMsgError(
   ElMessage({
     type: 'error',
     message,
+    duration,
     showClose: true,
     plain,
     dangerouslyUseHTMLString,
