f3bddd8944cced9e5c68340e63a14df0dcc30b6f
Yuzu <1581523609@qq.com>
2024-11-09 17:49:36 +0800

    feat(stores): 添加用户相关数据持久化管理
    - 新增 user store 模块，用于管理用户信息和 token
    - 实现了设置 token 和用户信息的方法
    - 配置了数据持久化，使用本地存储保存用户相关数据

---
+++ src/stores/modules/user.ts
@@ -0,0 +1,49 @@
+import { PINIA_PREFIX_KEY } from '@/config'
+import type { UserInfo } from '@/types/system/user'
+import { defineStore } from 'pinia'
+
+const useUserStore = defineStore('user', {
+  state: (): State => ({
+    token: '',
+    userInfo: {
+      userId: 0,
+      deptId: 0,
+      userName: '',
+      nickName: '',
+      userType: 0,
+      phone: '',
+      email: '',
+      sex: 0,
+      avatar: '',
+      status: 0,
+      delFlag: 0,
+      loginIp: '',
+      loginDate: '',
+      sortNum: 0,
+      createBy: '',
+      createTime: '',
+      updateBy: '',
+      updateTime: '',
+      remark: '',
+    },
+  }),
+  actions: {
+    setToken(token: string) {
+      this.token = token
+    },
+    setUserInfo(userInfo: UserInfo) {
+      this.userInfo = userInfo
+    },
+  },
+  persist: {
+    key: PINIA_PREFIX_KEY + 'user',
+    storage: localStorage,
+  },
+})
+
+export default useUserStore
+
+interface State {
+  token: string
+  userInfo: UserInfo
+}
