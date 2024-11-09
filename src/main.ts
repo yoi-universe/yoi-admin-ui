99623908e198ccc17717e70cb5edf8302472c3dc
Yuzu <1581523609@qq.com>
2024-11-09 13:44:41 +0800

    feat: 注册 Element Plus 全集图标
    - 引入 @element-plus/icons-vue 包
    - 遍历并注册所有图标组件
    - 在应用中全局可用 Element Plus 图标

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router'
 import pinia from '@/stores'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 /**
  * css样式
  */
@@ -11,6 +13,11 @@ import '@/styles/reset.scss'
 import 'virtual:uno.css'
 
 const app = createApp(App)
+
+// 注册element plus 所有图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(pinia)
 app.use(router)
