3c4e8e99fc20dc8926da750416282acdd1dde581
Yuzu <1581523609@qq.com>
2024-11-09 17:49:26 +0800

    feat(types): 添加用户信息接口
    新增 UserInfo 接口，用于定义用户信息的类型结构。包含以下字段：
    - userId: 用户ID
    - deptId: 部门ID
    - userName: 用户名
    - nickName: 昵称
    - userType: 账号类型
    - phone: 手机号码
    - email: 邮箱
    - sex: 性别
    - avatar: 头像
    - status: 状态
    - delFlag: 删除标志
    - loginIp: 登录IP
    - loginDate: 登录时间
    - sortNum: 排序
    - createBy: 创建者
    - createTime: 创建时间
    - updateBy: 更新者
    - updateTime: 更新时间
    - remark: 备注

---
+++ src/types/system/user.ts
@@ -0,0 +1,61 @@
+/**
+ * 用户信息
+ */
+export interface UserInfo {
+  /* 用户id */
+  userId: number
+
+  /* 部门id */
+  deptId: number
+
+  /* 用户名 */
+  userName: string
+
+  /* 昵称 */
+  nickName: string
+
+  /* 账号类型 */
+  userType: number
+
+  /* 手机号码 */
+  phone: string
+
+  /* 邮箱 */
+  email: string
+
+  /* 性别 */
+  sex: number
+
+  /* 头像 */
+  avatar: string
+
+  /* 状态 */
+  status: number
+
+  /* 删除标志 */
+  delFlag: number
+
+  /* 登录ip */
+  loginIp: string
+
+  /* 登录时间 */
+  loginDate: string
+
+  /* 排序 */
+  sortNum: number
+
+  /* 创建者 */
+  createBy: string
+
+  /* 创建时间 */
+  createTime: string
+
+  /* 更新者 */
+  updateBy: string
+
+  /* 更新时间 */
+  updateTime: string
+
+  /* 备注 */
+  remark: string
+}
