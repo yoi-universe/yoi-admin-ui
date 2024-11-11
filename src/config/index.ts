a46911573c5c1eab47d41edefc2673699c66eb73
Yuzu <1581523609@qq.com>
2024-11-11 15:15:11 +0800

    feat(config): 添加登录页地址和白名单地址配置
    - 新增 LOGIN_URL 常量，用于指定登录页地址
    - 新增 WHITE_URL_LIST 常量，用于指定白名单地址

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,9 @@
 // 全局默认配置
 // pinia仓库前缀
 export const PINIA_PREFIX_KEY: string = 'yoi-'
+
+// 登录页地址
+export const LOGIN_URL: string = '/login'
+
+// 白名单地址
+export const WHITE_URL_LIST: string[] = ['/404', '/403']
