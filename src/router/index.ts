ce3898ddcfe9b531c645ee381d2aa5f456fa00d0
Yuzu <1581523609@qq.com>
2024-11-11 15:15:47 +0800

    feat(router): 添加路由守卫和动态路由功能
    - 增加了前置路由守卫，用于处理登录状态和权限验证
    - 添加了后置路由守卫，用于结束进度条
    - 实现了动态路由的加载和添加
    - 集成了错误处理机制
    - 优化了页面标题的设置

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,22 @@ import {
   createRouter,
   createWebHashHistory,
   createWebHistory,
+  type NavigationGuardNext,
+  type RouteLocationNormalized,
+  type RouteLocationNormalizedLoaded,
   type RouterHistory,
 } from 'vue-router'
 import { errorRouter, layoutRouter } from './modules/staticRouter'
+import { useAuthStore, useUserStore } from '@/stores'
+import nProgress from 'nprogress'
+import getPageTitle from '@/utils/getPageTitle'
+import { LOGIN_URL, WHITE_URL_LIST } from '@/config'
+import { elMsgError } from '@/utils/elMsg'
+import { initDynamicRouter } from './modules/dynamicRouter'
+
+nProgress.configure({
+  showSpinner: false, // 关闭加载图标
+})
 
 // .env 配置文件读取
 const mode = import.meta.env.VITE_ROUTER_MODE
@@ -22,6 +35,77 @@ const router = createRouter({
     return { left: 0, top: 0 }
   },
 })
+
+/**
+ * 前置路由
+ */
+router.beforeEach(
+  async (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalizedLoaded,
+    next: NavigationGuardNext,
+  ) => {
+    const userStore = useUserStore()
+    const authStore = useAuthStore()
+
+    // 进度条开始
+    nProgress.start()
+
+    // 页面标题
+    document.title = getPageTitle(to.meta)
+
+    // 判断访问的是否是登录页
+    if (to.path === LOGIN_URL) {
+      // 判断是否有token
+      if (userStore.token) {
+        return next(from.fullPath)
+      } else {
+        elMsgError('您未登录，或者登录已经超时，请先登录！')
+      }
+      // 重置路由
+      authStore.getMenuList.forEach(route => {
+        const { routeName } = route
+        if (routeName && router.hasRoute(routeName)) {
+          router.removeRoute(routeName)
+        }
+      })
+      return next()
+    }
+
+    // 判断访问的是否是白名单地址
+    if (WHITE_URL_LIST.includes(to.path)) return next()
+
+    // 判断是否有token
+    if (!userStore.token) return next({ path: LOGIN_URL, replace: true })
+
+    // 判断是否有菜单，没有就初始化
+    if (!authStore.getMenuList.length) {
+      await initDynamicRouter()
+      return next({ ...to, replace: true })
+    }
+
+    next()
+  },
+)
+
+/**
+ * 跳转错误
+ */
+router.onError(error => {
+  // 结束进度条
+  nProgress.done()
+  console.warn('路由错误', error)
+})
+
+/**
+ * 后置路由
+ */
+router.afterEach(
+  (to: RouteLocationNormalized, from: RouteLocationNormalizedLoaded) => {
+    // 结束进度条
+    nProgress.done()
+  },
+)
 
 export default router
 
--- src/router/modules/dynamicRouter.ts
+++ src/router/modules/dynamicRouter.ts
@@ -1,1 +1,59 @@
-export const initDynamicRouter = () => {}
+import { getUserInfoApi, getUserMenuApi } from '@/api/auth'
+import { useAuthStore, useUserStore } from '@/stores'
+import router from '..'
+import { LOGIN_URL } from '@/config'
+
+export const initDynamicRouter = async () => {
+  const modules = import.meta.glob('@/views/**/*.vue')
+
+  const authStore = useAuthStore()
+  const userStore = useUserStore()
+  try {
+    // 获取路由
+    const res = await Promise.all([getUserMenuApi(), getUserInfoApi()])
+    const menuList = res[0].data
+    const userInfo = res[1].data
+
+    authStore.setMenuList(menuList)
+    authStore.setRoles(userInfo.roles)
+    authStore.setPermissions(userInfo.permissions)
+    userStore.setUserInfo(userInfo.loginUser)
+
+    // 判断是否有菜单权限
+    if (!authStore.getMenuList.length) {
+      userStore.setToken('')
+      router.replace(LOGIN_URL)
+      return
+    }
+
+    // 添加动态路由
+    authStore.getMenuList.forEach(item => {
+      const componentString = item.component.replace(/^\/+/, '') // 过滤字符串前面所有 '/' 字符
+      const componentPath = componentString.replace(/\.\w+$/, '') // 过滤掉后缀名，为了让 import 加入 .vue ，不然会有警告提示...
+      const componentUrl = '/src/' + componentPath + '.vue'
+
+      const route = {
+        path: item.path,
+        name: item.routeName,
+        component: modules[componentUrl],
+        meta: {
+          parentId: item.parentId,
+          title: item.menuName,
+          icon: item.icon,
+          visible: item.visible,
+          isCache: item.isCache,
+          isLink: item.isLink,
+          linkType: item.linkType,
+          linkUrl: item.linkUrl,
+        },
+      }
+      router.addRoute('layout', route)
+    })
+  } catch (error) {
+    console.log(error)
+    // 获取用户登录信息请求出错时
+    userStore.setToken('')
+    router.replace(LOGIN_URL)
+    return Promise.reject(error)
+  }
+}
