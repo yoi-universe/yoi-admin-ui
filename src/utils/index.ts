3dff460c8386b762adea4cd89f886ec420077a30
Yuzu <1581523609@qq.com>
2024-11-15 14:26:48 +0800

    feat(utils): 添加递归构建树形结构函数
    - 新增 recursiveTree 函数，用于将数组数据转换为树形结构
    - 函数支持自定义父节点 ID、节点 ID 和子节点字段名
    - 优化了性能，避免了不必要的循环和判断

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,43 @@
 export function getAssets(url: string) {
   return new URL(`../assets/${url}`, import.meta.url).href
 }
+
+/**
+ * 递归构建树形结构
+ *
+ * @param items 数组类型的数据
+ * @param parentId 当前节点的父节点ID，可选
+ * @param parentIdField 父节点ID的字段名，默认为'parentId'
+ * @param idField 当前节点的ID字段名，默认为'id'
+ * @param childrenField 子节点的字段名，默认为'children'
+ * @returns 返回树形结构数组
+ */
+export function recursiveTree<T, R extends T>(
+  items: T[],
+  parentId: number | number[],
+  parentIdField = 'parentId',
+  idField = 'menuId',
+  childrenField = 'children',
+) {
+  const result: R[] = []
+  for (const item of items) {
+    if (
+      (item as any)[parentIdField] === parentId ||
+      (Array.isArray(parentId) &&
+        parentId.includes((item as any)[parentIdField] as never))
+    ) {
+      const children: R[] = recursiveTree<T, R>(
+        items,
+        (item as any)[idField],
+        parentIdField,
+        idField,
+        childrenField,
+      )
+      if (children.length) {
+        ;(item as any)[childrenField] = children
+      }
+      result.push(item as R)
+    }
+  }
+  return result
+}
