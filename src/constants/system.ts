48dbcba7d19ae5255126674a5e35bcd0aa80be79
Yuzu <1581523609@qq.com>
2024-11-15 14:25:59 +0800

    feat(constants): 添加菜单类型和连接打开方式相关常量
    - 新增菜单类型常量：目录、菜单、按钮
    - 新增菜单是否为外链常量
    - 新增连接打开方式常量：内部、外部

---
+++ src/constants/system.ts
@@ -0,0 +1,12 @@
+// 菜单类型 [0:目录;1:菜单;2:按钮]
+export const MENU_TYPE_DIRECTORY = 0
+export const MENU_TYPE_MENU = 1
+export const MENU_TYPE_BUTTON = 2
+
+// 菜单是否为外链 [0:否;1:是]
+export const MENU_NOT_LINK = 0
+export const MENU_IS_LINK = 1
+
+// 连接打开方式 [0:内部;1:外部]
+export const MENU_LINK_TARGET_SELF = 0
+export const MENU_LINK_TARGET_BLANK = 1
