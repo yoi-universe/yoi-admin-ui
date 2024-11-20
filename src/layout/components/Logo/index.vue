aa5b68f6516593ab6f83808a86886798d0fcf80e
Yuzu <1581523609@qq.com>
2024-11-20 16:47:40 +0800

    refactor(layout): 优化 logo 组件
    - 为 logo 图片添加 alt 属性
    - 添加 isCollapse 属性，根据侧边栏状态显示/隐藏 logo 文本
    - 调整 logo 样式，包括圆角、位置等
    - 使用 truncate 类限制 logo 文本长度

--- src/layout/components/Logo/index.vue
+++ src/layout/components/Logo/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="logo">
-    <img class="border-rd-50%" :src="logo" width="34" height="34">
-    <div class="logo-text flex-1 font-size-18px font-bold ml-10px">
+    <img class="rounded-50% ml-9px" :src="logo" width="34" height="34" alt="avatar">
+    <div class="logo-text flex-1 font-size-18px font-bold ml-10px truncate" v-show="!isCollapse">
       YOI-ADMIN
     </div>
   </div>
@@ -11,6 +11,13 @@
 import { getAssets } from '@/utils';
 
 const logo = getAssets('images/logo.svg')
+const { isCollapse } = defineProps<Props>()
+
+</script>
+<script lang="ts">
+interface Props {
+  isCollapse: boolean
+}
 </script>
 
 <style lang="scss" scoped>
@@ -18,6 +25,7 @@ const logo = getAssets('images/logo.svg')
   height: $aside-header-height;
   display: flex;
   align-items: center;
+  position: relative;
 
   .logo-text {
     color: var(--el-color-primary);
