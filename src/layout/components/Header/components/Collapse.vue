ec2bab25f909c36885d67f3650975186f928b277
Yuzu <1581523609@qq.com>
2024-11-20 16:48:34 +0800

    feat(layout): 添加侧边栏折叠组件
    - 实现了一个可点击的折叠按钮，用于切换侧边栏的展开和折叠状态
    - 使用了 Element Plus 的图标组件和 Pinia 的状态管理
    - 组件样式和交互逻辑均已包含

---
+++ src/layout/components/Header/components/Collapse.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="w-36px h-36px rounded-5px hover:bg-[--el-color-primary-light-9] group flex flex-justify-center flex-items-center"
+    @click="handleCollapse">
+    <el-icon size="20" class="group-hover:color-[--el-color-primary]">
+      <Expand v-if="isCollapse" />
+      <Fold v-else />
+    </el-icon>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useGlobalStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+
+const globalStore = useGlobalStore()
+const { isCollapse } = storeToRefs(globalStore)
+const { setCollapse } = globalStore
+
+const handleCollapse = () => {
+  setCollapse(!isCollapse.value)
+}
+</script>
+
+<style scoped></style>
