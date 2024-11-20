94bc2ee9a165eb8f8b09091d52107096196917c6
Yuzu <1581523609@qq.com>
2024-11-20 13:47:26 +0800

    feat(layout): 添加 logo 组件
    - 新增 logo.svg 文件作为应用 logo 图标
    - 实现 Logo 组件，包括 logo 图标和应用名称
    - 使用 getAssets 函数动态获取 logo 路径
    - 调整 logo 样式，设置为圆形并添加应用名称

--- src/layout/components/Logo/index.vue
+++ src/layout/components/Logo/index.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="logo">
-    logo
+    <img class="border-rd-50%" :src="logo" width="34" height="34">
+    <div class="logo-text flex-1 font-size-18px font-bold ml-10px">
+      YOI-ADMIN
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { getAssets } from '@/utils';
 
-<style scoped></style>
+const logo = getAssets('images/logo.svg')
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  height: $aside-header-height;
+  display: flex;
+  align-items: center;
+
+  .logo-text {
+    color: var(--el-color-primary);
+  }
+}
+</style>
