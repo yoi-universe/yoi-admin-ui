bfd5029eacf54e15bde6a5f387330de1dc6054da
Yuzu <1581523609@qq.com>
2024-11-21 15:34:59 +0800

    feat(layout): 优化面包屑组件并添加动画效果
    - 为面包屑添加过渡动画，提升用户体验
    - 优化面包屑样式，包括布局、颜色和图标
    - 增加面包屑点击事件，实现跳转到对应菜单项
    - 使用 computed 属性动态获取面包屑列表

--- src/layout/components/Header/components/Breadcrumb.vue
+++ src/layout/components/Header/components/Breadcrumb.vue
@@ -1,27 +1,81 @@
 <template>
-  <div>
+  <div class="breadcrumb">
     <el-breadcrumb :separator-icon="ArrowRight">
-      <el-breadcrumb-item v-for="item in breadcrumbList" :key="item.menuId">
-        <div>
-          <el-icon size="18">
-            <component :is="item.icon"></component>
-          </el-icon>
-          <span>{{ item.menuName }}</span>
-        </div>
-      </el-breadcrumb-item>
+      <transition-group name="breadcrumb">
+        <el-breadcrumb-item v-for="item in breadcrumbList" :key="item.menuId">
+          <div class="el-breadcrumb__inner" @click="handleBreadcrumb(item)">
+            <el-icon size="18">
+              <component :is="item.icon"></component>
+            </el-icon>
+            <span class="el-breadcrumb__text">{{ item.menuName }}</span>
+          </div>
+        </el-breadcrumb-item>
+      </transition-group>
     </el-breadcrumb>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useAuthStore } from '@/stores';
+import type { MenuTree } from '@/types/system/menu';
 import { ArrowRight } from '@element-plus/icons-vue'
-import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute()
 const authStore = useAuthStore()
-const breadcrumbList = authStore.getBreadcrumb(route.path)
+const breadcrumbList = computed(() => {
+  return authStore.getBreadcrumb(route.path)
+})
 
+const router = useRouter()
+const handleBreadcrumb = (item: MenuTree) => {
+  if (item.children && item.children.length) {
+    const firstRouter = item.children[0]
+    router.push(firstRouter.path)
+  }
+}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+// 进入动画的生效状态
+.breadcrumb-enter-active {
+  transition: all 0.3s;
+}
+
+// 进入动画的起始状态和离开动画的生效状态
+.breadcrumb-enter-from,
+.breadcrumb-leave-active {
+  opacity: 0;
+  transform: translateX(10px);
+}
+
+.breadcrumb {
+  padding: 0 50px 0 0;
+  margin-left: 10px;
+  overflow: hidden;
+  user-select: none;
+
+  .el-breadcrumb {
+    &__item {
+      &:last-child {
+        .el-breadcrumb__inner:hover {
+          color: var(--el-text-color-regular);
+        }
+      }
+    }
+
+    &__inner {
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+
+      &:hover {
+        color: var(--el-color-primary);
+      }
+    }
+  }
+
+
+}
+</style>
