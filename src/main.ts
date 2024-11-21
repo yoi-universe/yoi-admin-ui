c817f36f0c28e51dd30b082de04f1efe0e814b87
Yuzu <1581523609@qq.com>
2024-11-21 17:23:20 +0800

    style: 添加 Element Plus 深色模式样式
    - 引入了 Element Plus 的 dark 样式文件
    - 为了支持应用中的深色模式，增强了用户体验

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import i18n from '@/languages'
 import '@/styles/reset.scss' // 重置样式
 import 'nprogress/nprogress.css' // 进度条
 import 'element-plus/theme-chalk/el-message.css' // 消息提示样式
+import 'element-plus/theme-chalk/dark/css-vars.css' // element plus dark 样式
 import 'virtual:uno.css' // UnoCss
 
 /**
