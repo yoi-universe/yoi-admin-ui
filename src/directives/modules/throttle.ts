8a1dea9cf6606c916fc433bcf54fd07997cc445a
Yuzu <1581523609@qq.com>
2024-12-05 16:52:12 +0800

    feat(directives): 调整节流默认延迟时间并添加注释
    - 将节流指令的默认延迟时间从 1000 毫秒调整为 500 毫秒
    - 为节流指令添加注释，说明其功能和参数

--- src/directives/modules/throttle.ts
+++ src/directives/modules/throttle.ts
@@ -1,8 +1,12 @@
 import type { Directive, DirectiveBinding } from 'vue'
 
+/**
+ * 节流
+ * @param 延迟时间
+ */
 const throttle: Directive = {
   mounted(el: HTMLElement, binding: DirectiveBinding) {
-    const delay = parseInt(binding.arg || '1000')
+    const delay = parseInt(binding.arg || '500')
     const fn = binding.value
     let lastTime = 0
 
