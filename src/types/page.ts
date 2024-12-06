5a8183dd7cc1153f7cd9a217f7ccc217c1d88b12
Yuzu <1581523609@qq.com>
2024-12-06 17:32:06 +0800

    feat(types/page): 添加 PageInfo 接口
    - 在 page.ts 文件中新增 PageInfo 接口
    - 定义了分页信息相关的属性，包括总数、数据列表、当前页等
    - 添加了与分页导航相关的属性，如上一页、下一页、是否第一页等

--- src/types/page.ts
+++ src/types/page.ts
@@ -5,3 +5,59 @@ export interface PageParams {
   isAsc?: string
   reasonable?: boolean
 }
+
+export interface PageInfo<T> {
+  /* 总数 */
+  total: number
+
+  /* 数据列表 */
+  list: T[]
+
+  /* 当前页 */
+  pageNum: number
+
+  /* 每页的数量 */
+  pageSize: number
+
+  /* 总页数 */
+  size: number
+
+  /* 起始行 */
+  startRow: number
+
+  /* 结束行 */
+  endRow: number
+
+  /* 总页数 */
+  pages: number
+
+  /* 上一页 */
+  prePage: number
+
+  /* 下一页 */
+  nextPage: number
+
+  /* 是否为第一页 */
+  isFirstPage: boolean
+
+  /* 是否为最后一页 */
+  isLastPage: boolean
+
+  /* */
+  hasPreviousPage: boolean
+
+  /* */
+  hasNextPage: boolean
+
+  /* */
+  navigatePages: number
+
+  /* */
+  navigatepageNums: string[]
+
+  /* */
+  navigateFirstPage: number
+
+  /* */
+  navigateLastPage: number
+}
