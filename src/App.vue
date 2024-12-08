715c23c66c831b6c9536c676287069017dea9e8f
Yuzu <1581523609@qq.com>
2024-12-08 15:35:29 +0800

    feat(i18n): 实现国际化支持
    - 引入 vue-i18n 和全局存储
    - 动态设置语言环境
    - 使用 el-config-provider 包裹应用以支持国际化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useGlobalStore } from './stores'
 
+const i18n = useI18n()
+const globalStore = useGlobalStore()
+
+const locale = computed(() => {
+  return (i18n.getLocaleMessage(globalStore.language) as any).langConfig
+})
 </script>
 
 <template>
-  <RouterView />
+  <el-config-provider :locale="locale">
+    <RouterView />
+  </el-config-provider>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
