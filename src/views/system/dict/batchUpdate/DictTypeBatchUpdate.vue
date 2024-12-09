8fcba337878c2bc1088541ac0a9d5e694aadde0b
Yuzu <1581523609@qq.com>
2024-12-09 14:32:18 +0800

    feat(system): 新增字典类型页面功能
    - 添加字典类型列表页面，支持搜索、新增、修改、删除等操作
    - 实现字典类型数据的批量更新功能
    - 优化字典类型数据的展示和操作体验

---
+++ src/views/system/dict/batchUpdate/DictTypeBatchUpdate.vue
@@ -0,0 +1,173 @@
+<template>
+  <YoiDialog
+    ref="yoiDialogRef"
+    title="修改字典类型"
+    :loading="loading"
+    :btn-loading="btnLoading"
+    @confirm="handleConfirm"
+    @cancel="handleCancel"
+    :confirm-text="submitText"
+  >
+    <template #content>
+      <el-form ref="formRef" :rules="rules" :model="form" label-width="auto">
+        <el-row>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }">
+            <!-- 字典名称 -->
+            <el-form-item label="字典名称" prop="dictName">
+              <el-input
+                v-model="form.dictName"
+                placeholder="请输入字典名称"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }" class="pl-10px">
+            <!-- 字典类型 -->
+            <el-form-item label="字典类型" prop="dictType">
+              <el-input
+                v-model="form.dictType"
+                placeholder="请输入字典类型"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }">
+            <!-- 字典状态 -->
+            <el-form-item label="字典状态" prop="status">
+              <el-select v-model="form.status" placeholder="请选择字典状态">
+                <el-option label="启用" :value="0" />
+                <el-option label="停用" :value="1" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }" class="pl-10px">
+            <el-form-item label="排序" prop="sortNum">
+              <el-input-number v-model="form.sortNum" clearable />
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 24 }">
+            <!-- 字典备注 -->
+            <el-form-item label="字典备注" prop="remark">
+              <el-input
+                v-model="form.remark"
+                :rows="5"
+                type="textarea"
+                placeholder="请输入字典备注"
+              />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <!--  -->
+      </el-form>
+    </template>
+  </YoiDialog>
+</template>
+
+<script lang="ts" setup>
+import { computed, reactive, ref } from 'vue'
+import YoiDialog from '@/components/YoiDialog/index.vue'
+import type { FormInstance } from 'element-plus'
+import type { UpdateDictTypeInfoParams } from '@/api/system/dict/type'
+import {
+  getDictTypeInfoApi,
+  updateDictTypeApi,
+} from '@/api/system/dict/dictType'
+import { elMsgSuccess } from '@/utils/elMsg'
+
+const currentIndex = ref(0)
+const idList = ref<number[]>([])
+const submitText = computed(() => {
+  return currentIndex.value === idList.value.length - 1
+    ? '确定'
+    : '确定并编辑下一页'
+})
+
+const loading = ref(false)
+const btnLoading = ref(false)
+const formRef = ref<FormInstance>()
+const form = ref<UpdateDictTypeInfoParams>({
+  dictId: 0,
+  dictName: '',
+  dictType: '',
+  status: 0,
+  sortNum: 0,
+  remark: '',
+})
+const rules = reactive({
+  dictName: [{ required: true, message: '请输入字典名称', trigger: 'blur' }],
+  dictType: [{ required: true, message: '请输入字典类型', trigger: 'blur' }],
+  status: [{ required: true, message: '请选择状态', trigger: 'blur' }],
+  sortNum: [{ required: true, message: '请输入排序', trigger: 'blur' }],
+})
+
+const getData = async () => {
+  loading.value = true
+  const dictTypeInfoRes = await getDictTypeInfoApi(
+    idList.value[currentIndex.value],
+  ).catch(e => e)
+  if (dictTypeInfoRes.code === 200) {
+    form.value = dictTypeInfoRes.data
+  }
+  loading.value = false
+}
+
+const resetForm = () => {
+  currentIndex.value = 0
+  idList.value = []
+  form.value = {
+    dictId: 0,
+    dictName: '',
+    dictType: '',
+    status: 0,
+    sortNum: 0,
+    remark: '',
+  }
+  if (formRef.value) {
+    formRef.value.resetFields()
+  }
+}
+const yoiDialogRef = ref()
+const emit = defineEmits(['confirm'])
+const handleConfirm = () => {
+  if (!formRef.value) return
+  formRef.value.validate(async valid => {
+    if (valid) {
+      btnLoading.value = true
+      console.log('submit!')
+      const result = await updateDictTypeApi(form.value).catch(e => e)
+      if (result.code === 200) {
+        if (currentIndex.value === idList.value.length - 1) {
+          yoiDialogRef.value.confirmClose()
+        } else {
+          elMsgSuccess('已提交')
+          currentIndex.value++
+          getData()
+        }
+        emit('confirm')
+      }
+      btnLoading.value = false
+    } else {
+      console.log('error submit!')
+    }
+  })
+}
+const handleCancel = () => {
+  yoiDialogRef.value.close()
+}
+const open = (ids: number[]) => {
+  yoiDialogRef.value.open()
+  resetForm()
+  idList.value = ids
+  getData()
+}
+
+defineExpose({
+  open,
+})
+</script>
+
+<style scoped></style>
---
+++ src/views/system/dict/type.vue
@@ -0,0 +1,331 @@
+<template>
+  <div class="yoi-flex">
+    <YoiCard>
+      <!-- 搜索条件 -->
+      <el-form :inline="true" v-show="showSearch">
+        <el-form-item label="字典名称" prop="dictName">
+          <el-input
+            v-model="searchParams.dictName"
+            placeholder="请输入字典名称"
+            clearable
+            style="width: 240px"
+          />
+        </el-form-item>
+
+        <el-form-item label="字典类型" prop="dictType">
+          <el-input
+            v-model="searchParams.dictType"
+            placeholder="请输入字典类型"
+            clearable
+            style="width: 240px"
+          />
+        </el-form-item>
+
+        <el-form-item label="字典状态" prop="status">
+          <el-select
+            placeholder="请选择字典状态"
+            v-model.number="searchParams.status"
+            clearable
+            style="width: 240px"
+          >
+            <el-option label="启用" :value="0" />
+            <el-option label="停用" :value="1" />
+          </el-select>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button
+            type="primary"
+            icon="Search"
+            plain
+            v-throttle="handleSearch"
+            >搜索</el-button
+          >
+          <el-button type="danger" icon="refresh" plain v-debounce="resetSearch"
+            >重置</el-button
+          >
+        </el-form-item>
+      </el-form>
+      <!-- 表格头部按钮 -->
+      <el-row :gutter="10">
+        <el-col :span="1.5">
+          <el-button type="primary" icon="Plus" plain @click="handleAdd"
+            >新增</el-button
+          >
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="success"
+            icon="Edit"
+            plain
+            :disabled="multiple"
+            @click="handleBatchUpdate"
+            >修改</el-button
+          >
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="danger"
+            icon="Delete"
+            plain
+            :disabled="multiple"
+            @click="handleBatchDelete"
+            >删除</el-button
+          >
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="danger"
+            icon="Refresh"
+            plain
+            @click="handleRefreshCache"
+            >刷新缓存</el-button
+          >
+        </el-col>
+        <YoiToolbar v-model:show-search="showSearch" @refresh-table="getData" />
+      </el-row>
+      <br />
+      <!-- 数据表格 -->
+      <el-table
+        v-loading="loading"
+        :data="tableList"
+        border
+        row-key="menuId"
+        :tree-props="{ children: 'children' }"
+        empty-text="暂时没有数据"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" align="center" width="55" />
+        <el-table-column
+          prop="dictName"
+          label="字典名称"
+          align="center"
+          width="160"
+        />
+        <el-table-column
+          prop="dictType"
+          label="字典类型"
+          align="center"
+          width="160"
+        >
+          <template #default="{ row }">
+            <el-link
+              type="primary"
+              :underline="false"
+              @click="handleView(row)"
+              >{{ row.dictType }}</el-link
+            >
+          </template>
+        </el-table-column>
+        <el-table-column prop="status" label="状态" align="center" width="100">
+          <template #default="{ row }">
+            <el-tag type="success" v-if="row.status === 0">启用</el-tag>
+            <el-tag type="danger" v-if="row.status === 1">停用</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="remark"
+          label="备注"
+          align="center"
+          width="200"
+          show-overflow-tooltip
+        />
+        <el-table-column
+          prop="sortNum"
+          label="排序"
+          align="center"
+          width="90"
+        />
+        <el-table-column
+          prop="createTime"
+          label="创建时间"
+          align="center"
+          width="180"
+        />
+        <el-table-column
+          prop="updateTime"
+          label="修改时间"
+          align="center"
+          width="180"
+        />
+        <el-table-column
+          prop="updateBy"
+          label="修改人"
+          align="center"
+          width="160"
+        />
+        <el-table-column label="操作" align="center" width="180" fixed="right">
+          <template #default="{ row }">
+            <el-tooltip content="修改" placement="top">
+              <el-button
+                type="primary"
+                icon="Edit"
+                circle
+                plain
+                @click="handleUpdate(row)"
+              ></el-button>
+            </el-tooltip>
+            <el-tooltip content="删除" placement="top">
+              <el-button
+                type="danger"
+                icon="Delete"
+                circle
+                plain
+                @click="handleDelete(row)"
+              ></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <br />
+      <!-- 分页 -->
+      <el-pagination
+        background
+        v-model:current-page="searchParams.pageNum"
+        v-model:page-size="searchParams.pageSize"
+        v-show="total > 0"
+        :page-sizes="[10, 20, 50, 100, 200]"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        @size-change="getData"
+        @current-change="getData"
+      />
+    </YoiCard>
+
+    <DictTypeAdd ref="addRef" @confirm="getData" />
+    <DictTypeUpdate ref="updateRef" @confirm="getData" />
+    <DictTypeBatchUpdate ref="batchUpdateRef" @confirm="getData" />
+  </div>
+</template>
+
+<script lang="ts" setup name="dictTypePage">
+import { onMounted, ref } from 'vue'
+import YoiCard from '@/components/YoiCard/index.vue'
+import YoiToolbar from '@/components/YoiToolbar/index.vue'
+import DictTypeAdd from './add/DictTypeAdd.vue'
+import DictTypeUpdate from './update/DictTypeUpdate.vue'
+import DictTypeBatchUpdate from './batchUpdate/DictTypeBatchUpdate.vue'
+import { elMsgError, elMsgSuccess, elMsgWarning } from '@/utils/elMsg'
+import { elMsgBox } from '@/utils/elMsgBox'
+import type { GetDictTypeListParams } from '@/api/system/dict/type'
+import type { DictTypeInfo } from '@/types/system/dict'
+import {
+  deleteDictTypeApi,
+  getDictTypeListApi,
+  refreshDictCacheApi,
+} from '@/api/system/dict/dictType'
+import { useRouter } from 'vue-router'
+
+const searchParams = ref<GetDictTypeListParams>({
+  dictName: '',
+  dictType: '',
+  status: null,
+  pageNum: 1,
+  pageSize: 10,
+})
+const total = ref(0)
+const showSearch = ref(true)
+// 数据表格加载页面动画
+const loading = ref(false)
+const ids = ref<number[]>([])
+const multiple = ref(true)
+const tableList = ref<DictTypeInfo[]>([])
+const handleSelectionChange = (val: DictTypeInfo[]) => {
+  ids.value = val.map(item => item.dictId)
+  multiple.value = !val.length
+}
+
+const getData = async () => {
+  loading.value = true
+  const dictTypeRes = await getDictTypeListApi(searchParams.value)
+  if (dictTypeRes.code === 200) {
+    tableList.value = dictTypeRes.data.list
+    total.value = dictTypeRes.data.total
+  } else {
+    elMsgError(dictTypeRes.message)
+  }
+  loading.value = false
+}
+
+const handleSearch = () => {
+  searchParams.value.pageNum = 1
+  getData()
+}
+
+const resetSearch = () => {
+  searchParams.value = {
+    dictName: '',
+    dictType: '',
+    status: null,
+    pageNum: 1,
+    pageSize: 10,
+  }
+  getData()
+}
+
+const router = useRouter()
+const handleView = (row: DictTypeInfo) => {
+  router.push(`/dict_data/${row.dictType}`)
+}
+
+const addRef = ref()
+const handleAdd = () => {
+  addRef.value.open()
+}
+
+const batchUpdateRef = ref()
+const handleBatchUpdate = () => {
+  batchUpdateRef.value.open(ids.value)
+}
+
+const handleBatchDelete = () => {
+  if (ids.value.length === 0) {
+    elMsgWarning('请选择要删除的数据')
+    return
+  }
+  elMsgBox('您确认进行删除么？')
+    .then(async () => {
+      await deleteDictTypeApi(ids.value)
+      elMsgSuccess('删除成功')
+      getData()
+    })
+    .catch(e => {
+      elMsgError(e.message || '已取消')
+    })
+}
+
+const handleRefreshCache = async () => {
+  const res = await refreshDictCacheApi().catch(e => e)
+  if (res.code === 200) {
+    elMsgSuccess('刷新成功')
+  }
+}
+
+const updateRef = ref()
+const handleUpdate = (row: DictTypeInfo) => {
+  updateRef.value.open(row.dictId)
+}
+const handleDelete = (row: DictTypeInfo) => {
+  const id = row.dictId
+  if (!id) {
+    elMsgWarning('请选择要删除的数据')
+    return
+  }
+  elMsgBox(`您确认需要删除菜单名称[${row.dictName}]吗？`)
+    .then(async () => {
+      await deleteDictTypeApi([id])
+      elMsgSuccess('删除成功')
+      getData()
+    })
+    .catch(e => {
+      elMsgError(e.message || '已取消')
+    })
+}
+
+onMounted(() => {
+  getData()
+})
+</script>
+
+<style scoped></style>
