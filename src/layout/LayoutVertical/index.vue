b89cac8d540537f75fbefed970482d9929afe799
Yuzu <1581523609@qq.com>
2024-12-09 16:39:41 +0800

    feat(layout): 优化垂直布局并添加菜单激活状态逻辑
    - 格式化代码，提高可读性
    - 添加路由元信息 activeMenu 判断，优化菜单激活状态逻辑

--- src/layout/LayoutVertical/index.vue
+++ src/layout/LayoutVertical/index.vue
@@ -1,10 +1,17 @@
 <template>
   <el-container class="layout-container">
-    <el-aside class="layout-aside transition-all"
-      :width="!isCollapse ? `${menuWidth}px` : `${asideMenuCollapseWidth}px`">
+    <el-aside
+      class="layout-aside transition-all"
+      :width="!isCollapse ? `${menuWidth}px` : `${asideMenuCollapseWidth}px`"
+    >
       <Logo :isCollapse="isCollapse" />
       <el-scrollbar class="layout-scrollbar">
-        <el-menu :default-active="activeMenu" :collapse="isCollapse" :collapse-transition="false" :router="false">
+        <el-menu
+          :default-active="activeMenu"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="false"
+        >
           <AsideSubMenu :menuList="menuList"></AsideSubMenu>
         </el-menu>
       </el-scrollbar>
@@ -19,25 +26,26 @@
 </template>
 
 <script lang="ts" setup>
-import Header from '@/layout/components/Header/index.vue';
-import Logo from '@/layout/components/Logo/index.vue';
-import AsideSubMenu from '@/layout/components/Menu/AsideSubMenu.vue';
-import Main from '@/layout/components/Main/index.vue';
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-import { useAuthStore, useGlobalStore } from '@/stores';
-import { storeToRefs } from 'pinia';
+import Header from '@/layout/components/Header/index.vue'
+import Logo from '@/layout/components/Logo/index.vue'
+import AsideSubMenu from '@/layout/components/Menu/AsideSubMenu.vue'
+import Main from '@/layout/components/Main/index.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useAuthStore, useGlobalStore } from '@/stores'
+import { storeToRefs } from 'pinia'
 
 const globalStore = useGlobalStore()
 const { isCollapse } = storeToRefs(globalStore)
-const menuWidth = 220;
-const asideMenuCollapseWidth = 64;
+const menuWidth = 220
+const asideMenuCollapseWidth = 64
 const route = useRoute()
 const authStore = useAuthStore()
 
 const menuList = computed(() => authStore.getMenuTreeList)
-const activeMenu = computed(() => route.path)
-
+const activeMenu = computed(() =>
+  route.meta.activeMenu ? route.meta.activeMenu : route.path,
+)
 </script>
 
 <style lang="scss" scoped>
