5fa2afe2c1e0362cc851ab2cdf0676936d81d5c7
Yuzu <1581523609@qq.com>
2024-12-10 15:31:23 +0800

    fix(layout): 退出登录时清除 tabs 和字典缓存
    - 在 logout 函数中增加了 tabsStore 和 dictStore 的 $reset 方法调用
    - 优化了 User 组件的格式和结构
    - 添加了必要的导入和移除了未使用的导入

--- src/layout/components/Header/components/User.vue
+++ src/layout/components/Header/components/User.vue
@@ -2,7 +2,9 @@
   <div class="user">
     <el-avatar :size="34" :src="userInfo.avatar" />
     <el-dropdown class="ml-10px" @command="handleCommand">
-      <div class="flex align-center cursor-pointer select-none color-[--el-color-primary]">
+      <div
+        class="flex align-center cursor-pointer select-none color-[--el-color-primary]"
+      >
         {{ userInfo.nickName }}
         <el-icon class="ml-5px">
           <arrow-down />
@@ -10,8 +12,12 @@
       </div>
       <template #dropdown>
         <el-dropdown-menu>
-          <el-dropdown-item :command="USER_DROP_DOWN_MENU_PROFILE">个人中心</el-dropdown-item>
-          <el-dropdown-item :command="USER_DROP_DOWN_MENU_LOGOUT">退出登录</el-dropdown-item>
+          <el-dropdown-item :command="USER_DROP_DOWN_MENU_PROFILE"
+            >个人中心</el-dropdown-item
+          >
+          <el-dropdown-item :command="USER_DROP_DOWN_MENU_LOGOUT"
+            >退出登录</el-dropdown-item
+          >
         </el-dropdown-menu>
       </template>
     </el-dropdown>
@@ -19,16 +25,26 @@
 </template>
 
 <script lang="ts" setup>
-import { logoutApi } from '@/api/auth';
-import { LOGIN_URL } from '@/config';
-import { USER_DROP_DOWN_MENU_LOGOUT, USER_DROP_DOWN_MENU_PROFILE } from '@/constants/system';
-import { useAuthStore, useUserStore } from '@/stores';
-import { storeToRefs } from 'pinia';
-import { useRouter } from 'vue-router';
+import { logoutApi } from '@/api/auth'
+import { LOGIN_URL } from '@/config'
+import {
+  USER_DROP_DOWN_MENU_LOGOUT,
+  USER_DROP_DOWN_MENU_PROFILE,
+} from '@/constants/system'
+import {
+  useAuthStore,
+  useDictStore,
+  useTabsStore,
+  useUserStore,
+} from '@/stores'
+import { storeToRefs } from 'pinia'
+import { useRouter } from 'vue-router'
 
 const userStore = useUserStore()
 const { userInfo } = storeToRefs(userStore)
 const authStore = useAuthStore()
+const tabsStore = useTabsStore()
+const dictStore = useDictStore()
 
 const router = useRouter()
 
@@ -37,6 +53,8 @@ const logout = async () => {
   await logoutApi()
   userStore.$reset()
   authStore.$reset()
+  tabsStore.$reset()
+  dictStore.$reset()
   router.replace(LOGIN_URL)
 }
 
@@ -45,10 +63,10 @@ const handleCommand = (command: string) => {
     case USER_DROP_DOWN_MENU_PROFILE:
       // TODO: 跳转个人中心
       router.push('/personage')
-      break;
+      break
     case USER_DROP_DOWN_MENU_LOGOUT:
       logout()
-      break;
+      break
   }
 }
 </script>
