1d1ada6601dae572473d1d29c963f08474b73381
Yuzu <1581523609@qq.com>
2024-12-10 16:15:19 +0800

    feat(system): 添加部门管理功能
    - 新增部门添加、编辑和删除功能
    - 实现部门树形结构展示和选择
    - 添加部门状态管理和排序功能
    - 集成表单验证和提示

---
+++ src/views/system/dept/index.vue
@@ -0,0 +1,276 @@
+<template>
+  <div class="yoi-flex">
+    <YoiCard>
+      <!-- 搜索条件 -->
+      <el-form :inline="true" v-show="showSearch">
+        <el-form-item label="部门名称" prop="deptName">
+          <el-input
+            v-model="searchParams.deptName"
+            placeholder="请输入部门名称"
+            clearable
+            style="width: 240px"
+          />
+        </el-form-item>
+
+        <el-form-item label="部门状态" prop="status">
+          <el-select
+            placeholder="请选择部门状态"
+            v-model.number="searchParams.status"
+            clearable
+            style="width: 240px"
+          >
+            <el-option label="正常" :value="0" />
+            <el-option label="停用" :value="1" />
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="联系电话" prop="phone">
+          <el-input
+            v-model="searchParams.phone"
+            placeholder="请输入联系电话"
+            clearable
+            style="width: 240px"
+          />
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" icon="Search" plain v-throttle="getData"
+            >搜索</el-button
+          >
+          <el-button type="danger" icon="refresh" plain v-debounce="resetSearch"
+            >重置</el-button
+          >
+        </el-form-item>
+      </el-form>
+      <!-- 表格头部按钮 -->
+      <el-row :gutter="10">
+        <el-col :span="1.5">
+          <el-button type="primary" icon="Plus" plain @click="handleAdd()"
+            >新增</el-button
+          >
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="success"
+            icon="Edit"
+            plain
+            :disabled="multiple"
+            @click="handleBatchUpdate"
+            >修改</el-button
+          >
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="danger"
+            icon="Delete"
+            plain
+            :disabled="multiple"
+            @click="handleBatchDelete"
+            >删除</el-button
+          >
+        </el-col>
+        <YoiToolbar v-model:show-search="showSearch" @refresh-table="getData" />
+      </el-row>
+      <br />
+      <!-- 数据表格 -->
+      <el-table
+        v-loading="loading"
+        :data="tableList"
+        border
+        row-key="menuId"
+        :tree-props="{ children: 'children' }"
+        empty-text="暂时没有数据"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" align="center" width="55" />
+        <el-table-column prop="deptName" label="部门名称" width="160" />
+        <el-table-column
+          prop="leader"
+          label="负责人"
+          align="center"
+          width="100"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="phone"
+          label="联系电话"
+          align="center"
+          width="120"
+        />
+        <el-table-column
+          prop="email"
+          label="邮箱"
+          align="center"
+          width="180"
+          show-overflow-tooltip
+        />
+        <el-table-column
+          prop="status"
+          label="菜单状态"
+          align="center"
+          width="100"
+        >
+          <template #default="{ row }">
+            <YoiTag :tag-options="normalDisableOptions" :value="row.status" />
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="remark"
+          label="备注"
+          align="center"
+          width="220"
+          show-overflow-tooltip
+        />
+        <el-table-column
+          prop="sortNum"
+          label="排序"
+          align="center"
+          width="90"
+        />
+        <el-table-column label="操作" align="center" width="160" fixed="right">
+          <template #default="{ row }">
+            <el-tooltip content="新增" placement="top">
+              <el-button
+                type="primary"
+                icon="CirclePlus"
+                circle
+                plain
+                @click="handleAdd(row)"
+              ></el-button>
+            </el-tooltip>
+            <el-tooltip content="修改" placement="top">
+              <el-button
+                type="success"
+                icon="Edit"
+                circle
+                plain
+                @click="handleUpdate(row)"
+              ></el-button>
+            </el-tooltip>
+            <el-tooltip content="删除" placement="top">
+              <el-button
+                type="danger"
+                icon="Delete"
+                circle
+                plain
+                @click="handleDelete(row)"
+              ></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+    </YoiCard>
+
+    <DeptAdd ref="addRef" @confirm="getData" />
+    <DeptUpdate ref="updateRef" @confirm="getData" />
+    <DeptBatchUpdate ref="batchUpdateRef" @confirm="getData" />
+  </div>
+</template>
+
+<script lang="ts" setup name="menuPage">
+import { onMounted, ref } from 'vue'
+import YoiCard from '@/components/YoiCard/index.vue'
+import YoiToolbar from '@/components/YoiToolbar/index.vue'
+import DeptAdd from './add/DeptAdd.vue'
+import DeptUpdate from './update/DeptUpdate.vue'
+import DeptBatchUpdate from './batchUpdate/DeptBatchUpdate.vue'
+import { elMsgError, elMsgSuccess, elMsgWarning } from '@/utils/elMsg'
+import { elMsgBox } from '@/utils/elMsgBox'
+import { useDictStore } from '@/stores'
+import YoiTag from '@/components/YoiTag/index.vue'
+import type { GetDeptListParams } from '@/api/system/dept/type'
+import type { DeptInfo } from '@/types/system/dept'
+import { deleteDeptApi, getDeptListApi } from '@/api/system/dept'
+
+const searchParams = ref<GetDeptListParams>({
+  deptName: '',
+  phone: '',
+  status: null,
+})
+const showSearch = ref(true)
+// 数据表格加载页面动画
+const loading = ref(false)
+const ids = ref<number[]>([])
+const multiple = ref(true)
+const tableList = ref<DeptInfo[]>([])
+const handleSelectionChange = (val: DeptInfo[]) => {
+  ids.value = val.map(item => item.deptId)
+  multiple.value = !val.length
+}
+
+const dictStore = useDictStore()
+const normalDisableOptions = ref([])
+const getData = async () => {
+  loading.value = true
+  normalDisableOptions.value = await dictStore.getDictData('sys_normal_disable')
+  const menuRes = await getDeptListApi(searchParams.value)
+  if (menuRes.code === 200) {
+    tableList.value = menuRes.data
+  } else {
+    elMsgError(menuRes.message)
+  }
+  loading.value = false
+}
+
+const resetSearch = () => {
+  searchParams.value = {
+    deptName: '',
+    phone: '',
+    status: null,
+  }
+  getData()
+}
+
+const addRef = ref()
+const handleAdd = (row?: DeptInfo) => {
+  addRef.value.open(row?.deptId)
+}
+
+const batchUpdateRef = ref()
+const handleBatchUpdate = () => {
+  batchUpdateRef.value.open(ids.value)
+}
+
+const handleBatchDelete = () => {
+  if (ids.value.length === 0) {
+    elMsgWarning('请选择要删除的数据')
+    return
+  }
+  elMsgBox('您确认进行删除么？')
+    .then(async () => {
+      await deleteDeptApi(ids.value)
+      elMsgSuccess('删除成功')
+      getData()
+    })
+    .catch(e => {
+      elMsgError(e.message || '已取消')
+    })
+}
+
+const updateRef = ref()
+const handleUpdate = (row: DeptInfo) => {
+  updateRef.value.open(row.deptId)
+}
+const handleDelete = (row: DeptInfo) => {
+  const id = row.deptId
+  if (!id) {
+    elMsgWarning('请选择要删除的数据')
+    return
+  }
+  elMsgBox(`您确认需要删除菜单名称[${row.deptName}]吗？`)
+    .then(async () => {
+      await deleteDeptApi([id])
+      elMsgSuccess('删除成功')
+      getData()
+    })
+    .catch(e => {
+      elMsgError(e.message || '已取消')
+    })
+}
+
+onMounted(() => {
+  getData()
+})
+</script>
+
+<style scoped></style>
---
+++ src/views/system/dept/update/DeptUpdate.vue
@@ -0,0 +1,228 @@
+<template>
+  <YoiDialog
+    ref="yoiDialogRef"
+    title="修改部门"
+    :loading="loading"
+    :btn-loading="btnLoading"
+    @confirm="handleConfirm"
+    @cancel="handleCancel"
+    :height="400"
+  >
+    <template #content>
+      <el-form ref="formRef" :rules="rules" :model="form" label-width="auto">
+        <el-row>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 24 }">
+            <!-- 上级部门 -->
+            <el-form-item label="上级部门" prop="parentId">
+              <el-cascader
+                placeholder="请选择上级部门"
+                v-model="form.parentId"
+                :options="deptTreeList"
+                :props="{
+                  expandTrigger: 'hover',
+                  emitPath: false,
+                  checkStrictly: true,
+                  label: 'deptName',
+                  value: 'deptId',
+                }"
+                filterable
+                clearable
+                style="width: 100%"
+              >
+                <template #default="{ node, data }">
+                  <span>{{ data.deptName }}</span>
+                  <span v-if="!node.isLeaf">
+                    ({{ data.children.length }})
+                  </span>
+                </template>
+              </el-cascader>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }">
+            <!-- 部门名称 -->
+            <el-form-item label="部门名称" prop="deptName">
+              <el-input
+                v-model="form.deptName"
+                placeholder="请输入部门名称"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }" class="pl-10px">
+            <!-- 负责人 -->
+            <el-form-item label="负责人" prop="leader">
+              <el-input
+                v-model="form.leader"
+                placeholder="请输入负责人"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }">
+            <!-- 联系电话 -->
+            <el-form-item label="联系电话" prop="phone">
+              <el-input
+                v-model="form.phone"
+                placeholder="请输入联系电话"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }" class="pl-10px">
+            <!-- 邮箱 -->
+            <el-form-item label="邮箱" prop="email">
+              <el-input
+                v-model="form.email"
+                placeholder="请输入邮箱"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }">
+            <!-- 部门状态 -->
+            <el-form-item label="部门状态" prop="status">
+              <el-select v-model="form.status" placeholder="请选择部门状态">
+                <el-option label="正常" :value="0" />
+                <el-option label="停用" :value="1" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 12 }" class="pl-10px">
+            <el-form-item label="排序" prop="sortNum">
+              <el-input-number v-model="form.sortNum" clearable />
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :xs="{ span: 24 }" :sm="{ span: 24 }">
+            <!-- 部门备注 -->
+            <el-form-item label="部门备注" prop="remark">
+              <el-input
+                v-model="form.remark"
+                :rows="5"
+                type="textarea"
+                placeholder="请输入部门备注"
+              />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <!--  -->
+      </el-form>
+    </template>
+  </YoiDialog>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import YoiDialog from '@/components/YoiDialog/index.vue'
+import type { FormInstance } from 'element-plus'
+import type { DeptTree } from '@/types/system/dept'
+import type { UpdateDeptInfoParams } from '@/api/system/dept/type'
+import {
+  getDeptInfoApi,
+  getDeptSelectListApi,
+  updateDeptApi,
+} from '@/api/system/dept'
+
+const loading = ref(false)
+const btnLoading = ref(false)
+const formRef = ref<FormInstance>()
+const form = ref<UpdateDeptInfoParams>({
+  deptId: 0,
+  parentId: 0,
+  deptName: '',
+  leader: '',
+  phone: '',
+  email: '',
+  status: 0,
+  sortNum: 0,
+  remark: '',
+})
+const rules = reactive({
+  parentId: [{ required: true, message: '请选择上级部门', trigger: 'blur' }],
+  deptName: [{ required: true, message: '请输入部门名称', trigger: 'blur' }],
+  leader: [{ required: true, message: '请输入负责人', trigger: 'blur' }],
+  phone: [
+    { required: true, message: '请输入手机号码', trigger: 'blur' },
+    { min: 11, max: 11, message: '请输入正确的手机号码', trigger: 'blur' },
+  ],
+  status: [{ required: true, message: '请选择状态', trigger: 'blur' }],
+  sortNum: [{ required: true, message: '请输入排序', trigger: 'blur' }],
+})
+const deptTreeList = ref<Partial<DeptTree>[]>([])
+
+const getData = async () => {
+  loading.value = true
+  const deptTreeListRes = await getDeptSelectListApi().catch(e => e)
+  if (deptTreeListRes.code === 200) {
+    deptTreeList.value = deptTreeListRes.data
+    deptTreeList.value.unshift({
+      deptId: 0,
+      deptName: '顶级菜单',
+      children: [],
+    })
+  }
+  const deptInfoRes = await getDeptInfoApi(form.value.deptId).catch(e => e)
+  if (deptInfoRes.code === 200) {
+    form.value = deptInfoRes.data
+  }
+  loading.value = false
+}
+
+const resetForm = () => {
+  form.value = {
+    deptId: 0,
+    parentId: 0,
+    deptName: '',
+    leader: '',
+    phone: '',
+    email: '',
+    status: 0,
+    sortNum: 0,
+    remark: '',
+  }
+  if (formRef.value) {
+    formRef.value.resetFields()
+  }
+}
+const yoiDialogRef = ref()
+const emit = defineEmits(['confirm'])
+const handleConfirm = () => {
+  if (!formRef.value) return
+  formRef.value.validate(async valid => {
+    if (valid) {
+      btnLoading.value = true
+      console.log('submit!')
+      const result = await updateDeptApi(form.value).catch(e => e)
+      if (result.code === 200) {
+        yoiDialogRef.value.confirmClose()
+        emit('confirm')
+      }
+      btnLoading.value = false
+    } else {
+      console.log('error submit!')
+    }
+  })
+}
+const handleCancel = () => {
+  yoiDialogRef.value.close()
+}
+const open = (id: number) => {
+  yoiDialogRef.value.open()
+  resetForm()
+  form.value.deptId = id
+  getData()
+}
+
+defineExpose({
+  open,
+})
+</script>
+
+<style scoped></style>
