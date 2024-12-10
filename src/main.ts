cbc55ef2779d1ccbe98402478554b633e0f0b882
Yuzu <1581523609@qq.com>
2024-12-10 22:06:40 +0800

    style: 添加 Element Plus 加载组件样式
    - 引入了 Element Plus 的 el-loading.css 文件
    - 此修改用于增强页面加载和数据加载时的用户体验

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ import '@/styles/index.scss' // 全局样式
 import 'nprogress/nprogress.css' // 进度条
 import 'element-plus/theme-chalk/el-message.css' // 消息提示样式
 import 'element-plus/theme-chalk/el-message-box.css'
+import 'element-plus/theme-chalk/el-loading.css'
 import 'element-plus/theme-chalk/dark/css-vars.css' // element plus dark 样式
 import 'virtual:uno.css' // UnoCss
 
