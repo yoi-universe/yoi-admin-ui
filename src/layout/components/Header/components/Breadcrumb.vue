4697af2616e508f3167d28bbb74e85206456e9dc
Yuzu <1581523609@qq.com>
2024-12-10 17:12:52 +0800

    feat(layout): 优化面包屑组件以支持 activeMenu
    - 添加对 activeMenu 的支持，用于处理当前激活的菜单项
    - 修改面包屑数据处理逻辑，以适应存在 activeMenu 的情况
    - 优化 handleBreadcrumb 方法，增加对子菜单的处理

--- src/layout/components/Header/components/Breadcrumb.vue
+++ src/layout/components/Header/components/Breadcrumb.vue
@@ -2,7 +2,7 @@
   <div class="breadcrumb">
     <el-breadcrumb :separator-icon="ArrowRight">
       <transition-group name="breadcrumb">
-        <el-breadcrumb-item v-for="item in breadcrumbList" :key="item.menuId">
+        <el-breadcrumb-item v-for="item in breadcrumbList" :key="item.path">
           <div class="el-breadcrumb__inner" @click="handleBreadcrumb(item)">
             <YoiGlobalIcon :icon="item.icon" size="18" />
             <span class="el-breadcrumb__text">{{ item.menuName }}</span>
@@ -14,21 +14,37 @@
 </template>
 
 <script lang="ts" setup>
-import { useAuthStore } from '@/stores';
-import type { MenuTree } from '@/types/system/menu';
+import { useAuthStore } from '@/stores'
+import type { MenuTree } from '@/types/system/menu'
 import { ArrowRight } from '@element-plus/icons-vue'
-import { computed } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import YoiGlobalIcon from '@/components/YoiGlobalIcon/index.vue';
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import YoiGlobalIcon from '@/components/YoiGlobalIcon/index.vue'
 
 const route = useRoute()
 const authStore = useAuthStore()
 const breadcrumbList = computed(() => {
-  return authStore.getBreadcrumb(route.path)
+  let breadcrumbData = authStore.getBreadcrumb(route.path)
+
+  // 存在activeMenu
+  if (route.meta.activeMenu) {
+    breadcrumbData = authStore.getBreadcrumb(route.meta.activeMenu as string)
+    breadcrumbData[breadcrumbData.length - 1] = {
+      parentId: route.meta.parentId as number,
+      menuName: route.meta.title as string,
+      path: route.path,
+      routeName: route.name as string,
+      activeMenu: route.meta.activeMenu as string,
+      isCache: route.meta.isCache as number,
+      icon: route.meta.icon as string,
+    }
+  }
+
+  return breadcrumbData
 })
 
 const router = useRouter()
-const handleBreadcrumb = (item: MenuTree) => {
+const handleBreadcrumb = (item: Partial<MenuTree>) => {
   if (item.children && item.children.length) {
     const firstRouter = item.children[0]
     router.push(firstRouter.path)
@@ -74,7 +90,5 @@ const handleBreadcrumb = (item: MenuTree) => {
       }
     }
   }
-
-
 }
 </style>
