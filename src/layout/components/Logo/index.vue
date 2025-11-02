c801d0aca5569ea79936afde73ecf39f6ed4c78d
yuzu <1581523609@qq.com>
2025-11-02 19:10:49 +0800

    fix(layout): 修复 Logo 组件中 YOI-ADMIN 文本显示问题
    - 调整 Logo 组件模板结构，确保文本在折叠状态下不显示
    - 优化 getAssets 工具函数中字符串拼接方式

--- src/layout/components/Logo/index.vue
+++ src/layout/components/Logo/index.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="logo">
-    <img class="rounded-50% ml-9px" :src="logo" width="34" height="34" alt="avatar">
-    <div class="color-[--el-color-primary] flex-1 font-size-18px font-bold ml-10px truncate" v-show="!isCollapse">
+    <img
+      class="rounded-50% ml-9px"
+      :src="logo"
+      width="34"
+      height="34"
+      alt="avatar"
+    />
+    <div
+      class="color-[--el-color-primary] flex-1 font-size-18px font-bold ml-10px truncate"
+      v-show="!isCollapse"
+    >
       YOI-ADMIN
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { getAssets } from '@/utils';
+import { getAssets } from '@/utils'
 
 const logo = getAssets('images/logo.svg')
 const { isCollapse } = defineProps<Props>()
-
 </script>
 <script lang="ts">
 interface Props {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,7 @@
  * @returns 返回资源文件在本地文件系统中的路径
  */
 export function getAssets(url: string) {
-  return new URL(`../assets/${url}`, import.meta.url).href
+  return new URL('../assets/' + url, import.meta.url).href
 }
 
 /**
